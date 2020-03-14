a3b397cb6659ac17b2bb7d2104fd493616e3f792
blue <skqksh@gmail.com>
2020-03-14 16:37:19 +0900

    점프 애니매에션 적용

--- Assets/Assets/Scripts/Player.cs
+++ Assets/Assets/Scripts/Player.cs
@@ -5,16 +5,22 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    private Rigidbody2D _rigidbody2;
+    private Rigidbody2D _rbody;
     [SerializeField]
     private float _jumpForce = 220f;
     [SerializeField]
     private float _speed = 2.5f;
 
+    private PlayerAnimation _playerAnim;
+
+    private SpriteRenderer _PlayerSprite;
+
     // Start is called before the first frame update
     void Start()
     {
-        _rigidbody2 = GetComponent<Rigidbody2D>();
+        _rbody = GetComponent<Rigidbody2D>();
+        _playerAnim = GetComponent<PlayerAnimation>();
+        _PlayerSprite = GetComponentInChildren<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -32,14 +38,32 @@ public class Player : MonoBehaviour
     {
 
         float move = Input.GetAxisRaw("Horizontal");
-        _rigidbody2.velocity = new Vector2(move * _speed, _rigidbody2.velocity.y);
+
+        if (move != 0)
+        {
+            _PlayerSprite.flipX = move < 0;
+        }
+
+        _playerAnim.Move(move);
+        _rbody.velocity = new Vector2(move * _speed, _rbody.velocity.y);
+
+
+        if (_rbody.velocity.y == 0)
+        {
+            _playerAnim.Jump(false);
+
+        }
     }
 
     void Jump()
     {
-        Debug.Log(_rigidbody2.velocity.y);
-        if (_rigidbody2.velocity.y == 0)
-            _rigidbody2.AddForce(Vector2.up * _jumpForce);
+        if (_rbody.velocity.y == 0)
+        {
+            _rbody.AddForce(Vector2.up * _jumpForce);
+            _playerAnim.Jump(true);
+        }
+          
+
     }
 
 }
---
+++ Assets/Assets/Scripts/PlayerAnimation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    private Animator _anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _anim = GetComponentInChildren<Animator>();
+    }
+
+    public void Move(float move)
+    {
+        _anim.SetFloat("Move", Math.Abs(move));
+    }
+
+    public void Jump(bool isJumping)
+    {
+        _anim.SetBool("Jumping", isJumping);
+    }
+}
