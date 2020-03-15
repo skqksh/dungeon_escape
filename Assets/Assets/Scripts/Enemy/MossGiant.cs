082d81a691bdb275b46ba1229c8ea090b1e688a0
blue <skqksh@gmail.com>
2020-03-15 23:17:33 +0900

    몬스터 추가 및 이동구현 작업중

---
+++ Assets/Assets/Scripts/Enemy/MossGiant.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MossGiant : Enemy
+{
+
+    private Vector3 _currentTarget;
+
+
+    public override void Attack()
+    {
+
+    }
+
+    public override void Update()
+    {
+        Movement();
+    }
+
+    private void Movement()
+    {
+        if (transform.position == pointA.position)
+        {
+            _currentTarget = pointB.position;
+        }
+        else if (transform.position == pointB.position)
+        {
+            _currentTarget = pointA.position;
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, _currentTarget, speed * Time.deltaTime);
+
+    }
+}
---
+++ Assets/Assets/Scripts/Player/Player.cs
@@ -0,0 +1,98 @@
+﻿using UnityEngine;
+using Debug = UnityEngine.Debug;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody2D _rbody;
+    [SerializeField]
+    private float _jumpForce = 220f;
+    [SerializeField]
+    private float _speed = 2.5f;
+
+    private PlayerAnimation _playerAnim;
+
+    private SpriteRenderer _playerSprite;
+
+    private SpriteRenderer _swordSprite;
+
+    private BoxCollider2D _boxCollider2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rbody = GetComponent<Rigidbody2D>();
+        _playerAnim = GetComponent<PlayerAnimation>();
+        _playerSprite = GetComponentInChildren<SpriteRenderer>();
+        _boxCollider2 = GetComponent<BoxCollider2D>();
+        _swordSprite = transform.GetChild(1).GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+    }
+
+    void Movement()
+    {
+
+        float move = Input.GetAxisRaw("Horizontal");
+
+        if (move != 0)
+        {
+            bool isFaceLeft = move < 0;
+            _playerSprite.flipX = isFaceLeft;
+            _swordSprite.flipX = isFaceLeft;
+            _swordSprite.flipY = isFaceLeft;
+        }
+
+        _playerAnim.Move(move);
+        _rbody.velocity = new Vector2(move * _speed, _rbody.velocity.y);
+
+
+        if (_rbody.velocity.y == 0)
+        {
+            _playerAnim.Jump(false);
+
+        }
+
+        if (IsGrounded())
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                _rbody.AddForce(Vector2.up * _jumpForce);
+                _playerAnim.Jump(true);
+            }
+            else if (Input.GetKeyDown(KeyCode.Z))
+            {
+                _playerAnim.Attack();
+            }
+        }
+
+    }
+
+
+    private bool IsGrounded()
+    {
+
+        float extraHeightText = 0.1f;
+        RaycastHit2D raycastHit = Physics2D.BoxCast(_boxCollider2.bounds.center, _boxCollider2.bounds.size, 0f, Vector2.down, extraHeightText, 1 << 8);
+
+        Color rayColor;
+        if (raycastHit.collider != null)
+        {
+            rayColor = Color.green;
+        }
+        else
+        {
+            rayColor = Color.red;
+        }
+        Debug.DrawRay(_boxCollider2.bounds.center + new Vector3(_boxCollider2.bounds.extents.x, 0), Vector2.down * (_boxCollider2.bounds.extents.y + extraHeightText), rayColor);
+        Debug.DrawRay(_boxCollider2.bounds.center - new Vector3(_boxCollider2.bounds.extents.x, 0), Vector2.down * (_boxCollider2.bounds.extents.y + extraHeightText), rayColor);
+        Debug.DrawRay(_boxCollider2.bounds.center - new Vector3(_boxCollider2.bounds.extents.x, _boxCollider2.bounds.extents.y + extraHeightText), Vector2.right * (_boxCollider2.bounds.extents.x * 2f), rayColor);
+
+        Debug.Log(raycastHit.collider);
+        return raycastHit.collider != null;
+    }
+
+}
