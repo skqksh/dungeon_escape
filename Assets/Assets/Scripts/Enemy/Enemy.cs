865f3e4946453165f5bd1d3bc82e131e1178dc26
blue <skqksh@gmail.com>
2020-03-18 00:01:13 +0900

    Skeleton enemy 추가

--- Assets/Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Assets/Scripts/Enemy/Enemy.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -13,10 +14,46 @@ public abstract class Enemy : MonoBehaviour
     [SerializeField]
     protected Transform pointA, pointB;
 
-    public virtual void Attack()
-    {
+    protected Vector3 currentTarget;
+    protected Animator animator;
+    protected  SpriteRenderer monsterSprite;
 
+    public virtual void Init()
+    {
+        animator = GetComponentInChildren<Animator>();
+        monsterSprite = GetComponentInChildren<SpriteRenderer>();
     }
 
-    public abstract void Update();
+    private void Start()
+    {
+        Init();
+    }
+
+    public virtual void Update()
+    {
+        if (animator.GetCurrentAnimatorStateInfo(0).IsName("Idle"))
+        {
+            return;
+        }
+
+        Movement();
+    }
+    public virtual  void  Movement()
+    {
+        monsterSprite.flipX = currentTarget == pointA.position;
+        
+        if (transform.position == pointA.position)
+        {
+            currentTarget = pointB.position;
+            animator.SetTrigger("Idle");
+        }
+        else if (transform.position == pointB.position)
+        {
+            currentTarget = pointA.position;
+            animator.SetTrigger("Idle");
+        }
+        transform.position = Vector3.MoveTowards(transform.position, currentTarget, speed * Time.deltaTime);
+
+    }
+    
 }
--- Assets/Assets/Scripts/Enemy/Spider.cs
+++ Assets/Assets/Scripts/Enemy/Spider.cs
@@ -6,46 +6,6 @@ using UnityEngine;
 public class Spider : Enemy
 {
 
-    private Vector3 _currentTarget;
-    private Animator _animator;
-    private SpriteRenderer _monsterSprite;
-    private void Start()
-    {
-        _animator = GetComponentInChildren<Animator>();
-        
-        _monsterSprite = GetComponentInChildren<SpriteRenderer>();
-    }
+   
 
-    public override void Attack()
-    {
-
-    }
-
-    public override void Update()
-    {
-        if (_animator.GetCurrentAnimatorStateInfo(0).IsName("Idle"))
-        {
-            return;
-        }
-        
-        Movement();
-    }
-
-    private void Movement()
-    {
-        _monsterSprite.flipX = _currentTarget == pointA.position;
-        
-        if (transform.position == pointA.position)
-        {
-            _currentTarget = pointB.position;
-            _animator.SetTrigger("Idle");
-        }
-        else if (transform.position == pointB.position)
-        {
-            _currentTarget = pointA.position;
-            _animator.SetTrigger("Idle");
-        }
-        transform.position = Vector3.MoveTowards(transform.position, _currentTarget, speed * Time.deltaTime);
-
-    }
 }
