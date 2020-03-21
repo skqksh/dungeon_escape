cfaf662afafec09500587d9ce4ff060a7d722fa9
blue <skqksh@gmail.com>
2020-03-21 23:02:36 +0900

    자이언트와 스파이더 힛박스 추가

--- Assets/Assets/Scripts/Enemy/Spider.cs
+++ Assets/Assets/Scripts/Enemy/Spider.cs
@@ -3,9 +3,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Spider : Enemy
+public class Spider  : Enemy, IDamageable
 {
+    public int Health { get; set; }
 
-   
+    public override void Init()
+    {
+        base.Init();
+        this.Health = health;
+    }
 
+    public void Damage(int damageAmount)
+    {
+        animator.SetTrigger("Hit");
+        --Health;
+        if (Health < 1)
+        {
+            Destroy(this.gameObject);
+        }
+
+        Debug.Log("ouch : " + damageAmount);
+    }
 }
